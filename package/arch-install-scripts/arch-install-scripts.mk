da8f977a19adfdbc615da1b4f38ac38ada7f1209
Gaël PORTAY <gael.portay@collabora.com>
2019-08-05 16:55:36 -0400

    package: add host-arch-install-scripts
    Signed-off-by: Gaël PORTAY <gael.portay@collabora.com>

---
+++ package/arch-install-scripts/arch-install-scripts.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# arch-install-scripts
+#
+################################################################################
+
+ARCH_INSTALL_SCRIPTS_VERSION = 28
+ARCH_INSTALL_SCRIPTS_SITE = $(call github,archlinux,arch-install-scripts,v$(ARCH_INSTALL_SCRIPTS_VERSION))
+ARCH_INSTALL_SCRIPTS_LICENSE = GPL2
+ARCH_INSTALL_SCRIPTS_LICENSE_FILES = COPYING
+
+define HOST_ARCH_INSTALL_SCRIPTS_INSTALL_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) PREFIX=$(HOST_DIR) install
+endef
+
+$(eval $(host-generic-package))
