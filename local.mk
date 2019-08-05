fefcf57e707ff927b050f27fe21db637274d4d6b
Gaël PORTAY <gael.portay@collabora.com>
2019-08-05 17:39:03 -0400

    package: add skeleton-pacstrap
    Note: ArchLinuxPOWER[1], the unofficial port of Archlinux to PowerPC,
    uses the GNU C Library userland and ships prebuilt PowerPC64 and
    PowerPC64 LE binary packages using both the ABI elfv2. See commit[2].
    
    [1]: https://archlinuxpower.org/
    [2]: https://github.com/gportay/iamroot/commit/d3e86cd38253a9d8d1b78ef201787affa1b792cf
    
    Signed-off-by: Gaël PORTAY <gael.portay@collabora.com>

--- local.mk
+++ local.mk
@@ -1,3 +1,13 @@
+# PowerPC64 big endian by default uses the elfv1 ABI, and PowerPC64 little
+# endian by default uses the elfv2 ABI. However, Arch POWER has decided to use
+# the elfv2 ABI for both, so we force the ABI for PowerPC64 big endian when the
+# selected C library is glibc.
+ifeq ($(BR2_TOOLCHAIN_USES_GLIBC)$(BR2_powerpc64),yy)
+ifeq ($(BR2_PACKAGE_SKELETON_PACSTRAP_ARCHPOWER_POWERPC64_CONFIGURATION_FILE),y)
+HOST_GCC_COMMON_CONF_OPTS += --with-abi=elfv2
+endif
+endif
+
 ifndef ZSTDCAT
 ZSTDCAT = zstdcat
 INFLATE.zst = $(ZSTDCAT)
