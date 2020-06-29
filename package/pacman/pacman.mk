994a3c3738639622201eff20761c86a733ff0db8
Gaël PORTAY <gael.portay@collabora.com>
2020-06-28 20:11:59 -0400

    package: add host-pacman
    Signed-off-by: Gaël PORTAY <gael.portay@collabora.com>

---
+++ package/pacman/pacman.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# pacman
+#
+################################################################################
+
+PACMAN_VERSION = 6.0.2
+PACMAN_SITE = https://sources.archlinux.org/other/pacman
+PACMAN_SOURCE = pacman-$(PACMAN_VERSION).tar.xz
+PACMAN_LICENSE = GPL-2.0
+PACMAN_LICENSE_FILES = COPYING
+
+HOST_PACMAN_DEPENDENCIES = host-libarchive host-libcurl
+HOST_PACMAN_CONF_OPTS = -Dldconfig=/usr/bin/ldconfig -Ddatarootdir=$(HOST_DIR)/share
+
+$(eval $(host-meson-package))
