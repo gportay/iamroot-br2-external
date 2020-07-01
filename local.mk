2cfe9f55e8c675ebe7a126e1eba409dd06e22faa
Gaël PORTAY <gael.portay@collabora.com>
2020-07-01 12:10:45 -0400

    package: add host-pacman-mirrorlist
    See the mirrorlist generator for Arch Linux[1], for Arch Linux ARM[2],
    Arch Linux RISC-V[3], Manjaro [4] and Arch Linux 32[5].
    
    [1]: https://www.archlinux.org/mirrorlist/
    [2]: https://archlinuxarm.org/about/mirrors
    [3]: https://riscv.mirror.pkgbuild.com/
    [4]: https://repo.manjaro.org/
    [5]: https://archlinux32.org/mirrorlist/
    
    Signed-off-by: Gaël PORTAY <gael.portay@collabora.com>

---
+++ local.mk
@@ -0,0 +1,4 @@
+ifndef ZSTDCAT
+ZSTDCAT = zstdcat
+INFLATE.zst = $(ZSTDCAT)
+endif
---
+++ package/pacman-mirrorlist/pacman-mirrorlist.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# pacman-mirrorlist
+#
+################################################################################
+
+PACMAN_MIRRORLIST_VERSION = 20230410-1
+PACMAN_MIRRORLIST_SITE = http://mirror.rackspace.com/archlinux/core/os/x86_64
+PACMAN_MIRRORLIST_SOURCE = pacman-mirrorlist-$(PACMAN_MIRRORLIST_VERSION)-any.pkg.tar.zst
+ifeq ($(BR2_aarch64)$(BR2_arm),y)
+PACMAN_MIRRORLIST_VERSION = 20230206-1
+PACMAN_MIRRORLIST_SITE = http://mirror.archlinuxarm.org/aarch64/core/
+PACMAN_MIRRORLIST_SOURCE = pacman-mirrorlist-$(PACMAN_MIRRORLIST_VERSION)-any.pkg.tar.xz
+endif
+ifeq ($(BR2_riscv),y)
+PACMAN_MIRRORLIST_VERSION = 20230111-1
+PACMAN_MIRRORLIST_SITE = http://riscv.mirror.pkgbuild.com/repo/core/
+endif
+PACMAN_MIRRORLIST_LICENSE = GPL-2.0
+PACMAN_MIRRORLIST_STRIP_COMPONENTS = 0
+
+define HOST_PACMAN_MIRRORLIST_INSTALL_CMDS
+	$(INSTALL) -D $(@D)/etc/pacman.d/mirrorlist $(HOST_DIR)/etc/pacman.d/mirrorlist
+endef
+
+$(eval $(host-generic-package))
