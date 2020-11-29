9571fde2268ad0cb99e3d6c5a0e7919bc33b6542
Gaël PORTAY <gael.portay@collabora.com>
2020-11-29 06:55:42 -0500

    external.mk: add rm-build and rm-target rules
    Signed-off-by: Gaël PORTAY <gael.portay@collabora.com>

--- external.mk
+++ external.mk
@@ -1,1 +1,14 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_IAMROOT_PATH)/package/*/*.mk))
+
+ifndef ($(BUILD_DIR),)
+.PHONY: rm-build
+rm-build:
+	rm -rf $(BUILD_DIR)/*/*
+
+ifndef ($(BASE_TARGET_DIR),)
+.PHONY: rm-target
+rm-target:
+	rm -rf $(BASE_TARGET_DIR)
+	rm -rf $(BUILD_DIR)/*/.stamp_target_installed
+endif
+endif
