85c22f77dd3d67d5631ccfa26a40a348e9250d74
Gaël PORTAY <gael.portay@rtone.fr>
2024-02-10 15:48:35 +0100

    package/archlinux-keyring: bump to 20240208
    Signed-off-by: Gaël PORTAY <gael.portay@rtone.fr>

--- package/archlinux-keyring/archlinux-keyring.mk
+++ package/archlinux-keyring/archlinux-keyring.mk
@@ -4,12 +4,17 @@
 #
 ################################################################################
 
-ARCHLINUX_KEYRING_VERSION = 20211028
-ARCHLINUX_KEYRING_SITE = https://sources.archlinux.org/other/archlinux-keyring
-ARCHLINUX_KEYRING_LICENSE = GPL-2.0
+ARCHLINUX_KEYRING_VERSION = 20240208
+ARCHLINUX_KEYRING_SITE = https://gitlab.archlinux.org/archlinux/archlinux-keyring/-/archive/$(ARCHLINUX_KEYRING_VERSION)
+ARCHLINUX_KEYRING_LICENSE = GPL-3.0+
+HOST_ARCHLINUX_KEYRING_DEPENDENCIES = host-pkgconf host-python3 host-sequoia-sq
+
+define HOST_ARCHLINUX_KEYRING_BUILD_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) PREFIX=$(HOST_DIR) build
+endef
 
 define HOST_ARCHLINUX_KEYRING_INSTALL_CMDS
-	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) PREFIX=$(HOST_DIR) install
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) PREFIX=$(HOST_DIR) SYSTEMD_SYSTEM_UNIT_DIR=$(HOST_DIR)/usr/lib/systemd/system install
 endef
 
 $(eval $(host-generic-package))
