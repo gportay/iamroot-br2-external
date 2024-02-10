87c8fdc37979e9316d50727b796e3808d8ac0838
Gaël PORTAY <gael.portay@rtone.fr>
2024-02-10 13:55:40 +0100

    Add Arch Linux and its derivatives defconfigs
    Signed-off-by: Gaël PORTAY <gael.portay@rtone.fr>

---
+++ defconfig.mk
@@ -0,0 +1,40 @@
+#!/usr/bin/env -S make -f
+#
+# Copyright 2024 Gaël PORTAY
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+
+.PHONY: all
+all: update-defconfig
+
+.PHONY: update-defconfig
+update-defconfig:
+	$(foreach defconfig,$(wildcard configs/*_defconfig), \
+		$(MAKE) -f Makefile O=$(CURDIR)/output-$@ $(notdir $(defconfig)) savedefconfig && \
+	) \
+	echo "completed!"
+
+.PHONY: defconfigs
+defconfigs:
+	$(foreach defconfig,$(wildcard configs/*_defconfig), \
+		defconfig=$(notdir $(defconfig)) && \
+		$(MAKE) -f Makefile O=$(CURDIR)/output-$$defconfig $$defconfig && \
+	) \
+	echo "completed!"
+
+.PHONY: toolchain
+toolchain:
+	$(foreach defconfig,$(wildcard configs/*_defconfig), \
+		defconfig=$(notdir $(defconfig)) && \
+		$(MAKE) -f Makefile O=$(CURDIR)/output-$$defconfig $$defconfig && \
+		$(MAKE) -f Makefile O=$(CURDIR)/output-$$defconfig $@ && \
+	) \
+	echo "completed!"
+
+.PHONY: test-pacman.conf
+test-pacman.conf:
+	$(foreach defconfig,$(wildcard configs/*_defconfig), \
+		test -e package/skeleton-pacstrap/$(subst _defconfig,_pacman.conf,$(notdir $(defconfig))) && \
+	) \
+	echo "completed!"
